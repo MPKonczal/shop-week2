63226591c58ea385f6c73fea500b5b5f34917310
Mateusz Konczal <mateuszkonczal96@gmail.com>
2019-09-24 17:21:52 +0200

    Started project

---
+++ src/main/java/pl/edu/utp/wtie/homeworkweek2/HomeworkWeek2Application.java
@@ -0,0 +1,13 @@
+package pl.edu.utp.wtie.homeworkweek2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkWeek2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkWeek2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/utp/wtie/homeworkweek2/HomeworkWeek2ApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.edu.utp.wtie.homeworkweek2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomeworkWeek2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
